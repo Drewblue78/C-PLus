636f54ace0d013611e24aba84251e57bf7960c13
Andrew Power <andrewjosephpower@gmail.com>
2021-03-08 18:49:11 -0400

    intial commit

---
+++ Chapters/Chapter1/Ex5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int changeChar()
+{
+  char c;
+  cout << "char default value is: " << c << '\n';
+  cout << "Enter a character";
+  cin >> c;
+  cout << '  ' << c << ' ' << c << ' ' << c << '\n';
+  cout << ' ' << c << '   ' << c << '\n';
+  cout << c << '\n';
+  cout << c << '\n';
+  cout << c << '\n';
+  cout << c << '\n';
+  cout << c << '\n';
+  cout << ' ' << c << '   ' << c << '\n';
+  cout << '  ' << c << ' ' << c << ' ' << c << '\n';
+  return
+}
