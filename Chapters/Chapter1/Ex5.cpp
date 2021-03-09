beb12d29e49553ccfb00ebdc3e7dc0802994e41b
Andrew Power <andrewjosephpower@gmail.com>
2021-03-09 11:57:19 -0400

    chapter1 done

--- Chapters/Chapter1/Ex5.cpp
+++ Chapters/Chapter1/Ex5.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 using namespace std;
+
 void changeChar()
 {
   char c;
   cout << "char default value is: " << c << '\n';
   cout << "Enter a character";
   cin >> c;
-  cout << '  ' << c << ' ' << c << ' ' << c << '\n';
-  cout << ' ' << c << '   ' << c << '\n';
+  cout << "  " << c << ' ' << c << ' ' << c << '\n';
+  cout << ' ' << c << "    " << c << '\n';
   cout << c << '\n';
   cout << c << '\n';
   cout << c << '\n';
   cout << c << '\n';
   cout << c << '\n';
-  cout << ' ' << c << '   ' << c << '\n';
-  cout << '  ' << c << ' ' << c << ' ' << c << '\n';
+  cout << ' ' << c << "    " << c << '\n';
+  cout << "  " << c << ' ' << c << ' ' << c << '\n';
+}
+int main()
+{
+  changeChar();
 }
