91dd8db7513608405afdbbbec8d04ff60371a567
Andrew Power <andrewjosephpower@gmail.com>
2021-03-10 18:04:06 -0400

    changes in chapter3

---
+++ Chapters/Chapter3/Ex6.cpp
@@ -0,0 +1,12 @@
+// F^b = V * y;
+// sphere (4/3) Pi r^2;
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main(){
+    double buoyantForce, volumeObject, weighFluid,
+    // if (buoyantForce =>){
+
+};
+}
