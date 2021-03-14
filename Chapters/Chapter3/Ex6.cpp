891a470d56d5f02229d95a80d63dee683344c04a
Andrew Power <andrewjosephpower@gmail.com>
2021-03-14 19:09:20 -0300

    finish chapter 3 start chapter 4

--- Chapters/Chapter3/Ex6.cpp
+++ Chapters/Chapter3/Ex6.cpp
@@ -7,9 +7,21 @@ using namespace std;
 int main() {
   char ans;
   do {
-    double buoyantForce, volumeObject, weighFluid;
-    // if (buoyantForce =>){
+    double weight, radius, Fb;
+    // User input
+    cout << "Please enter the weight in pounds:";
+    cin >> weight;
+    cout << "Please enter the radius in feet: ";
+    cin >> radius;
 
+    // Compute  buoyant force
+    Fb = 62.4 * ((4 * 3.14 * radius * radius * radius) / 3);
+
+    // Will it float or sink?
+    if (Fb >= weight)
+      cout << "The sphere will float in water \n";
+    else
+      cout << "The sphere will sink in water \n";
     cout << "Do you want to begin again?";
     cin >> ans;
   } while (ans == 'y' || ans == 'Y');
--- Chapters/Chapter4/Ex1.cpp
+++ Chapters/Chapter4/Ex1.cpp
@@ -1,17 +1,27 @@
-#include <cmath>
 #include <iostream>
 using namespace std;
+double milesPerGallon(int ml, int lt);
+const double LITER = 0.264172;
 
 int main() {
-  double mpg() { const }
-
-  char choice() {
-    do {
-      while (mpg()) {
-        cout << "Sorry, that is not a valid choice." << endl;
-        cout << "Please try again." << endl << endl << endl;
-      }
-      cout << "Would you like to restart ? y/n .\n" << endl;
-      cin >> choice;
-    } while (choice == 'y');
-  }
+  char ans;
+  int lt, ml;
+  do {
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout << "Please enter the number of liters of gasoline:" << endl;
+    cin >> lt;
+    cout << "Please enter the number of miles traveled: " << endl;
+    cin >> ml;
+    cout << "The mileage is: " << milesPerGallon(ml, lt) << " mpg" << endl;
+    cout << "Do you want to begin again?";
+    cin >> ans;
+  } while (ans == 'y' || ans == 'Y');
+  cout << "Have a nice day. \n";
+  return 0;
+}
+double milesPerGallon(int m, int l) {
+  double gallons;
+  gallons = LITER * l;
+  return (m / gallons);
+}
