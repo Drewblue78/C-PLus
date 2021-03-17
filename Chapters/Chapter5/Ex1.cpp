99555b974e03bae5f4630ebc6143ab3984162371
Andrew Power <andrewjosephpower@gmail.com>
2021-03-17 11:04:19 -0300

    chapter 5 Ex 1 done

--- Chapters/Chapter5/Ex1.cpp
+++ Chapters/Chapter5/Ex1.cpp
@@ -3,40 +3,38 @@
 using namespace std;
 double calcAvg(double s1, double s2, double s3, double s4);
 double calcStandardDeviation(double s1, double s2, double s3, double s4,
-                             double avg)
+                             double avg);
 
-    void main() {
-  void calc() {
-    double s1, s2, s3, s4, avg, sdeviation, standardDeviation;
-    char ans;
-    do {
-      cout << "Enter s1:";
-      cin >> s1;
-      cout << "Enter s2:";
-      cin >> s2;
-      cout << "Enter s3:";
-      cin >> s3;
-      cout << "Enter s4:";
-      cin >> s4;
-      avg = calcAvg(s1, s2, s3, s4);
-      sdeviation = calcStandardDeviation(s1, s2, s3, s4, avg, 4);
-      cout << "Standard deviation:" << sdeviation << endl;
+int main() {
+  double s1, s2, s3, s4, avg, sdeviation, standardDeviation;
+  char ans;
+  do {
+    cout << "Enter s1:";
+    cin >> s1;
+    cout << "Enter s2:";
+    cin >> s2;
+    cout << "Enter s3:";
+    cin >> s3;
+    cout << "Enter s4:";
+    cin >> s4;
+    avg = calcAvg(s1, s2, s3, s4);
+    sdeviation = calcStandardDeviation(s1, s2, s3, s4, avg);
+    cout << "Standard deviation:" << sdeviation << endl;
 
-      cout << "Do you want to begin again?";
-      cin >> ans;
-    } while (ans == 'y' || ans == 'Y');
-    cout << "Have a nice day. \n";
-  }
-  double calcAverage(double s1, double s2, double s3, double s4) {
-    return (s1 + s2 + s3 + s4) / 4;
-  }
-  double calcStandardDeviation(double s1, double s2, double s3, double s4,
-                               double mean, int n) {
-    double sd;
-    sd = (pow((s1 - mean), 2) + pow((s2 - mean), 2) + pow((s3 - mean), 2) +
-          pow((s4 - mean), 2)) /
-         n;
-    sd = sqrt(sd);
-    return sd;
-  }
+    cout << "Do you want to begin again?";
+    cin >> ans;
+  } while (ans == 'y' || ans == 'Y');
+  cout << "Have a nice day. \n";
+}
+double calcAvg(double s1, double s2, double s3, double s4) {
+  return (s1 + s2 + s3 + s4) / 4;
+}
+double calcStandardDeviation(double s1, double s2, double s3, double s4,
+                             double mean) {
+  double sd;
+  sd = (pow((s1 - mean), 2) + pow((s2 - mean), 2) + pow((s3 - mean), 2) +
+        pow((s4 - mean), 2)) /
+       4;
+  sd = sqrt(sd);
+  return sd;
 }
--- Chapters/Chapter5/Ex2.cpp
+++ Chapters/Chapter5/Ex2.cpp
@@ -1,47 +1,43 @@
+#include <math.h>
+
 #include <cmath>
 #include <iostream>
 using namespace std;
 
-void inputImperial(double ft, double in);
-void calc(double ft, double in, double meters, double cm, double tempMeters,
-          double tempCentimeters);
-void outputMetric(double meters, double cm, double ft, double in,
-                  double tempMeters, double tempCentimeters);
+void giveImperial();
+void inputImperial(double& ft, double& in);
+void calcToMetric(double& ft, double& in, double& meters, double& cm);
+void outputMetric(double& meters, double& cm, double& ft, double& in);
 
-int main() {
-  int giveImperial() {
-    double ft, in, meters, cm;
-    char ans;
-    do {
-      cout << " Feet and inches to meters and centimeters" << endl;
-      inputImperial(ft, in);
-      calc(ft, in, meters, cm, tempMeters, tempCentimeters);
-      outputMetric(meters, cm, ft, in);
-      cout << "Do you want to begin again?" << endl;
-      cin >> ans;
-    } while (ans == 'y' || ans == 'Y');
-    cout << "Have a nice day. \n";
-    return 0;
-  }
-  void inputImperial(double ft, double in) {
-    cout << "Feet: ";
-    cin >> ft;
-    cout << "Inches: ";
-    cin >> in;
-  }
-  void calc(double ft, double in, double meters, double cm, double tempMeters,
-            double tempCentimeters) {
-    // double ft = meters / 0.3048;
-    // double in = meters / 0.0254;
-    // cm = 100 * meters;
-    // meters = ft / 0.3048;
-    tempMeters = (ft + (in / 12.0)) * 0.3048;
-    meters = tempMeters;
-    tempCentimeters = (tempMeters * 100) - 100;
-    cm = tempCentimeters;
-  }
-  void outputMetric(double ft, double in, double meters, double cm) {
-    cout << ft << " feet " << in << " inches is equivalent to " << meters
-         << " meters " << cm << " centimeters" << endl;
-  }
+int main() { giveImperial(); }
+void giveImperial() {
+  double ft, in, meters, cm;
+  char ans;
+  do {
+    cout << "Feet and inches to meters and centimeters" << endl;
+    inputImperial(ft, in);
+    calcToMetric(ft, in, meters, cm);
+    outputMetric(meters, cm, ft, in);
+    cout << "Do you want to begin again?" << endl;
+    cin >> ans;
+  } while (ans == 'y' || ans == 'Y');
+  cout << "Have a nice day. \n";
 }
+void inputImperial(double& ft, double& in) {
+  cout << "Feet: ";
+  cin >> ft;
+  cout << "Inches: ";
+  cin >> in;
+}
+void calcToMetric(double& ft, double& in, double& meters, double& cm) {
+  meters = % .1lf, floor(ft + (in / 12.0)) * 0.3048;
+  cm = (meters * 100) - 100;
+  // meters = ft * 0.3048;
+  // cm = in * 2.54;
+}
+void outputMetric(double& ft, double& in, double& meters, double& cm) {
+  cout << meters << " feet " << cm << " inches is equivalent to " << (floor(ft))
+       << " meters " << in << " centimeters" << endl;
+}
+// format, 2.3,round( 2.3),floor( 2.3),ceil( 2.3),trunc( 2.3));
+// % .1lf\n ", floor (3.8)
