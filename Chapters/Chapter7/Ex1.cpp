a4edaf9265ac695842bb4eade6d610e2f2055b50
Andrew Power <andrewjosephpower@gmail.com>
2021-03-18 19:58:17 -0300

    working on chapter 7

--- Chapters/Chapter7/Ex1.cpp
+++ Chapters/Chapter7/Ex1.cpp
@@ -4,17 +4,23 @@ using namespace std;
 int firstLast2(int arr[], int n);
 int main() {
   int n;
-  cout << "Number of Elements: ";
-  cin >> n;
-  int myarray[n];
-  for (int i = 0; i < n; i++) {
-    cin >> myarray[i];
-  }
+  char ans;
+  do {
+    cout << "Number of Elements: ";
+    cin >> n;
+    int myarray[n];
+    for (int i = 0; i < n; i++) {
+      cin >> myarray[i];
+    }
 
-  firstLast2(myarray, n);
-  return 0;
+    firstLast2(myarray, n);
+    return 0;
+
+    cout << "Do you want to begin again?";
+    cin >> ans;
+  } while (ans == 'y' || ans == 'Y');
+  cout << "Have a nice day. \n";
 }
-
 int firstLast2(int arr[], int n) {
   if (arr[0] == 2 || arr[n - 1] == 2) {
     cout << "True";
---
+++ Chapters/Chapter7/Ex2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+const int DECLARED_SIZE;
+
+void countNum2(int a[], int size, int amount);
+int search(const int a[], int target);
+
+int main() {
+  int arr[DECLARED_SIZE], listSize, target;
+  countNum2(arr, DECLARED_SIZE, listSize, amount);
+
+  char ans;
+  int result;
+  do {
+    result = search(arr, listSize);
+    if (result == -1)
+      cout << " 2 is not on the list." << endl;
+    else
+      cout << " 2 is in the array: " << amount << "times" << endl;
+  }
+}
+void countNum2(int a[], int size) int search(const int a[], int target) {
+  int index = 0;
+}
+
+int index = 0;
+bool found = false;
+while ((!found) && (index < 2))
+  if (target == a[index]) return 0;
+
+cout << "Do you want to begin again?";
+cin >> ans;
+}
+while (ans == 'y' || ans == 'Y')
+  ;
+cout << "Have a nice day. \n";
