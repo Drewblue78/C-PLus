1604a673c9c2eb2979cb64fe3901541d329990c4
Andrew Power <andrewjosephpower@gmail.com>
2021-03-18 21:32:34 -0300

    working on ch 8

--- Chapters/Chapter7/Ex2.cpp
+++ Chapters/Chapter7/Ex2.cpp
@@ -13,23 +13,27 @@ int main() {
   int result;
   do {
     result = search(arr, listSize);
-    if (result == -1)
+    if (result == -1) {
       cout << " 2 is not on the list." << endl;
-    else
+    } else {
       cout << " 2 is in the array: " << amount << "times" << endl;
+    }
+
+    void countNum2(int a[], int size) int search(const int a[], int target) {
+      int index = 0;
+      bool found = false;
+      while ((!found) && (index < 2))
+        if (2 == a[index]) {
+          index++
+        } else {
+          return 0
+        }
+    }
+
+    return 0;
   }
-}
-void countNum2(int a[], int size) int search(const int a[], int target) {
-  int index = 0;
-}
-
-int index = 0;
-bool found = false;
-while ((!found) && (index < 2))
-  if (target == a[index]) return 0;
-
-cout << "Do you want to begin again?";
-cin >> ans;
+  cout << "Do you want to begin again?";
+  cin >> ans;
 }
 while (ans == 'y' || ans == 'Y')
   ;
